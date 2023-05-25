d939ca0ae19723c540e889dc4fe8221baa6b69b1
silvaleodantas@gmail.com <silvaleodantas@gmail.com>
2023-05-25 17:52:02 +0000

    Olá mundo do primeiro dia...
    Realizados os primeiros passos e comandos...

---
+++ dia01/olaMundo.sql
@@ -0,0 +1,7 @@
+-- Databricks notebook source
+SELECT 'Olá mundo!'
+
+-- COMMAND ----------
+
+SELECT * -- selecione todas as colunas da tabela abaixo
+FROM silver.pizza_query.pedido -- ordem: catalogo, database e tabela
